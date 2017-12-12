b0b67b9657340f835010cb58fb9f6a4993179a7d
Source <296476710@qq.com>
2017-12-12 19:33:43 +0800

    添加微擎模块基本数据

---
+++ site.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 手机闪光文字模块微站定义
+ *
+ * @author source0104
+ * @url 
+ */
+defined('IN_IA') or exit('Access Denied');
+
+class Source_sjsgwzModuleSite extends WeModuleSite {
+
+	public function doMobileShanguang() {
+		//这个操作被定义用来呈现 功能封面
+	}
+	public function doWebChakan() {
+		//这个操作被定义用来呈现 管理中心导航菜单
+	}
+
+}
